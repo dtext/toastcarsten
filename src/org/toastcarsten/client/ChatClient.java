d5ccc49f5a14f902f14c4dd8c68bffa3807964a5
dtext <textores.danny@gmail.com>
2015-11-22 20:05:50 +0100

    Start development on the client, add some TODOs

--- src/org/toastcarsten/client/ChatClient.java
+++ src/org/toastcarsten/client/ChatClient.java
@@ -1,9 +1,34 @@
 package org.toastcarsten.client;
 
-public class ChatClient {
+import org.toastcarsten.shared.IClient;
+
+import java.util.Collection;
+
+public class ChatClient implements IClient {
 
     public static void main(String[] args) {
-        //TODO implement chat client
-        System.out.println("Hello World!");
+        ChatClient c = new ChatClient();
+        // TODO create Chat Client (GUI)
+    }
+
+    @Override
+    public void print(String text) {
+        // TODO implement printing of regular messages
+    }
+
+    @Override
+    public void printHl(String text) {
+        // TODO implement printing of highlighted messages
+    }
+
+    @Override
+    public void recvUserlist(Collection<String> list) {
+        // TODO handle the userlist answer
+    }
+
+    @Override
+    public void handleNameError() {
+        printHl("The chosen name is already taken. Please choose another name.");
+        // TODO maybe do something here? If not, this could also be in the Protocol.
     }
 }
